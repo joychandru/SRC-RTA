40dfac8c61dcb0ec32a1880f8e1c42908513dd41
joy <joychandrasekaran.mca@gmail.com>
2017-05-03 23:14:28 +0530

    Update on 3rd May

--- app/src/main/java/com/insurance/app/GetLocation.java
+++ app/src/main/java/com/insurance/app/GetLocation.java
@@ -34,9 +34,9 @@ public class GetLocation implements LocationListener
     double latitude; // latitude
     double longitude; // longitude
     // The minimum distance to change Updates in meters
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 5; // 10 meters
     // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+    private static final long MIN_TIME_BW_UPDATES = 5000; // 1 minute
     // Declaring a Location Manager
     protected LocationManager locationManager;
 
